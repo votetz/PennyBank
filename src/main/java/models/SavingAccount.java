52ffebe1fa981200e193bad5ab75af67fb5376c6
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-05-22 23:04:56 +0300

    feat: add SavingAccount model

---
+++ src/main/java/models/SavingAccount.java
@@ -0,0 +1,15 @@
+package models;
+
+public class SavingAccount extends BankAccount {
+    private double interestRate;
+
+    public SavingAccount(String accountId, String ownerName, double balance, double interestRate) {
+        super(accountId, ownerName, balance);
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public String getAccountType() {
+        return "Saving";
+    }
+}
